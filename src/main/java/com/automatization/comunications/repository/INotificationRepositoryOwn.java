2e28601d0e39e40a1072d3dcd17811459aa05879
Brsebastian1 <sebbarrera0@gmail.com>
2026-03-07 12:54:48 -0500

    Created model

--- src/main/java/com/automatization/comunications/repository/INotificationRepositoryOwn.java
+++ src/main/java/com/automatization/comunications/repository/INotificationRepositoryOwn.java
@@ -1,5 +1,13 @@
 package com.automatization.comunications.repository;
 
-public interface INotificationRepository {
+import java.time.DayOfWeek;
+import java.util.List;
+
+import com.automatization.comunications.model.ContractDto;
+import com.automatization.comunications.model.NotificationDto;
+
+public interface INotificationRepositoryOwn {
+    public List<ContractDto> findContractNextToPay(DayOfWeek datDayOfWeek);
+    public List<NotificationDto> findNotificationsById(String id);
 
 }
